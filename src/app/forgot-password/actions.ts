afd9f5d69d219c07bffe5d4c98eecc548d2d88dd
semutdev <root.semutdev@gmail.com>
2025-07-31 07:53:11 +0000

    saya tes daftar user baru tapi malah ke link ini: http://localhost:3000/

--- src/app/forgot-password/actions.ts
+++ src/app/forgot-password/actions.ts
@@ -1,7 +1,6 @@
 'use server'
 
 import { createClient } from '@/lib/supabase/server'
-import { redirect } from 'next/navigation'
 
 export async function resetPassword(formData: any) {
   const supabase = createClient()
